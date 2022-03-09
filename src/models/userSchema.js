9ea2f2019903a476d6c9a509411de6cf33276368
eduardo <eduardovinoles@gmail.com>
2022-03-09 16:23:54 -0300

    WIP

---
+++ src/models/userSchema.js
@@ -0,0 +1,31 @@
+var mongoose = require('mongoose');
+
+var Schema = mongoose.Schema;
+
+
+var userSchema = new Schema({
+    name: {
+        type: String,
+        required: true,
+    } ,
+    img: {
+        data: Buffer,
+        contentType: String
+    },
+    ci: {
+        type: String,
+        required: true,
+    } ,
+    tel: String,
+    address: String,
+    type: {
+        type: String,
+        required: true,
+    } 
+});
+
+// Compile model from schema
+var UserModel = mongoose.model('user', userSchema);
+
+module.exports = UserModel 
+
