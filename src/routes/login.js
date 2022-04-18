f0978c984da4ca8fc1bff9b24479352cc1606e1d
eduardo <eduardovinoles@gmail.com>
2022-04-17 21:42:16 -0300

    wip

--- src/routes/login.js
+++ src/routes/login.js
@@ -4,9 +4,13 @@ var router = express.Router();
 
 
 router.post('/login', async (req, res) => {
+    try {
     res.send({
         token: 'test123'
       });
+    } catch (error) {
+        res.send(error)
+    }
 })
 
 module.exports = router;
