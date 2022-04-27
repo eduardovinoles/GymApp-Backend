bfeba8390243a58ebaef18b30f05697ecd7934c6
eduardo <eduardovinoles@gmail.com>
2022-04-26 21:33:10 -0300

    wip

--- src/routes/login.js
+++ src/routes/login.js
@@ -3,15 +3,10 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/login', async (req, res) => {
-
-    const users = await UserModel.find({})
-    try {
+   
         res.send({
             token: 'test123'
           });
-    } catch (error) {
-        res.send(error)
-    }
-})
+        })
 
 module.exports = router;
